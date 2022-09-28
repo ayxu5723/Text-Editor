b4440cc1e7860b602465e2ca5f74919b34c3c96c
Shaitan5723 <105050107+Shaitan5723@users.noreply.github.com>
2022-09-28 19:22:40 +1000

    fix errors for getting data from database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,8 +12,8 @@ const initdb = async () =>
     },
   });
 
-
-export const postDb = async (content)  => {
+// PUT to the database
+export const putDb = async (content)  => {
   console.log('Post to the database');
 
   const jateDb = await openDB('jate', 1);
@@ -22,7 +22,7 @@ export const postDb = async (content)  => {
 
   const store = tx.objectStore('jate');
 
-  const request = store.add({ id: 1, value: content });
+  const request = store.put({ id: 1, value: content });
 
   // Get confirmation of the request.
   const result = await request;
@@ -30,26 +30,22 @@ export const postDb = async (content)  => {
 };
 ;
 
-// Export a function we will use to GET to the database.
+// GET from the database.
 export const getDb = async () => {
   console.log('GET from the database');
 
-  // Create a connection to the database database and version we want to use.
   const jateDb = await openDB('jate', 1);
 
-  // Create a new transaction and specify the database and data privileges.
   const tx = jateDb.transaction('jate', 'readonly');
 
-  // Open up the desired object store.
   const store = tx.objectStore('jate');
 
-  // Use the .getAll() method to get all data in the database.
   const request = store.get(1);
 
   // Get confirmation of the request.
   const result = await request;
-  console.log('result.value', result);
-  return result;
+  result? console.log('data retreived', result) : console.log('data not found');
+  return result?.value;
 };
 
 initdb();
